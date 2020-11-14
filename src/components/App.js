c5f5925a7e29419238ffaf009ae8b37c0a82eadf
Jye Quan <64167219+jqteh@users.noreply.github.com>
2020-11-14 12:32:47 +0000

    add latexconverter

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import LatexConverter from "./LatexConverter";
 
 function App() {
   const [notes, setNotes] = useState([]);
