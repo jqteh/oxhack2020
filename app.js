80492cc4bc61301afa7e8edb01168959bcc5e305
ben-kaye <benkaye001@gmail.com>
2020-11-14 16:18:05 +0000

    adding post

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config()
 // 'use strict';
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -13,20 +13,7 @@ const port = 3000;
 
 let subscriptionKey = process.env.COMPUTER_VISION_SUBSCRIPTION_KEY
 let endpoint = process.env.COMPUTER_VISION_ENDPOINT
-// if (!subscriptionKey) { throw new Error('Set your environment variables for your subscription key and endpoint.'); }
-
-var uriBase = endpoint + 'vision/v3.1/analyze';
-
-const imageUrl =
-    'https://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg';
-
-// Request parameters.
-const params = {
-    'visualFeatures': 'Categories,Description,Color',
-    'details': '',
-    'language': 'en'
-};
-
+if (!subscriptionKey) { throw new Error('Set your environment variables for your subscription key and endpoint.'); }
 
 
 
@@ -61,22 +48,29 @@ app.listen(port, () => {
 // app.route('/process/:imUrl')
 app.route('/process')
 
-.get(function(req, res) {
-  const imurl = req.params.imUrl
+.post(function(req, res) {
+  // const imurl = req.params.imUrl
+  imageUrl = req.body.url 
 
+  // const imageUrl =
+  //   'https://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg';
 
-  const imageUrl =
-    'https://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg';
-
-  console.log(imurl)
+  console.log(imageUrl)
   process_image(imageUrl)
 })
 
 
 function process_image(url) {
+  const method = 'vision/v3.1/read/analyze'
+  const uribase = endpoint + method
+  
+  // set language
+  const params = {
+    'language': 'en'
+  };
 
   const options = {
-    uri: uriBase,
+    uri: uribase,
     qs: params,
     body: '{"url": ' + '"' + url + '"}',
     headers: {
@@ -87,15 +81,40 @@ function process_image(url) {
 
   request.post(options, (error, response, body) => {
     if (error) {
-      console.log('Error: ', error);
+      console.log('Error: ', error);nod
       return;
     }
-    let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
-    console.log('JSON Response\n');
-    console.log(jsonResponse);
+
+    const req_id = response.headers['apim-request-id']
+
+    try_get(req_id)
   });
 }
 
+function try_get(id) {
+  req_url = endpoint + 'vision/v3.1/read/analyzeResults/' + id
+
+  const req_opt = { 
+      qs: '',
+      body: '',
+      headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': subscriptionKey
+    }
+  }
+
+const TIME_OUT = 50
+  
+request.get(req_url, req_opt, (error, response, body) => {
+    let jsonResponse = JSON.parse(body);
+    if (jsonResponse['status'] == 'running') {
+        setTimeout(try_get, id, TIME_OUT);
+    } else {
+        console.log('JSON Response\n');
+        console.log(JSON.stringify(jsonResponse));
+    }
+  })
+}
 
 
 
