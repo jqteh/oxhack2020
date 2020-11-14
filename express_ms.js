ce22dfaab535270d11bc662655bb7e1a1986be52
ben-kaye <benkaye001@gmail.com>
2020-11-14 14:19:49 +0000

    update

---
+++ express_ms.js
@@ -0,0 +1,44 @@
+// 'use strict';
+require('dotenv').config()
+const request = require('request');
+
+
+let subscriptionKey = process.env.COMPUTER_VISION_SUBSCRIPTION_KEY
+let endpoint = process.env.COMPUTER_VISION_ENDPOINT
+
+if (!subscriptionKey) { 
+    throw new Error('Set your environment variables for your subscription key and endpoint.');
+    // console.log(subscriptionKey);
+ }
+
+var uriBase = endpoint + 'vision/v3.1/analyze';
+
+const imageUrl =
+    'https://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg';
+
+// Request parameters.
+const params = {
+    'visualFeatures': 'Categories,Description,Color',
+    'details': '',
+    'language': 'en'
+};
+
+const options = {
+    uri: uriBase,
+    qs: params,
+    body: '{"url": ' + '"' + imageUrl + '"}',
+    headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key' : subscriptionKey
+    }
+};
+
+request.post(options, (error, response, body) => {
+  if (error) {
+    console.log('Error: ', error);nod
+    return;
+  }
+  let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+  console.log('JSON Response\n');
+  console.log(jsonResponse);
+});
