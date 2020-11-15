74ac6b74f53e785c8d38c0963f14d2e87b7cd50f
ben-kaye <benkaye001@gmail.com>
2020-11-15 20:44:12 +0000

    clean up repo

--- back_end/models/latexSc.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose")
-
-const latexSchema = mongoose.Schema({
-    latex: { type: String, required: true },
-});
-
-module.exports = mongoose.model('latexSc', latexSchema)
